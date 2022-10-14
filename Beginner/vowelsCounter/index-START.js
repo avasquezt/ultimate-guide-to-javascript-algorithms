c67b5a9b86da9a744c84cb3abbf9048ab4c4845c
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-14 13:35:46 -0500

    Add vowelsCounter  methods

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,11 +3,41 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
-
+// With filter method
 function vowelsCounter(text) {
-    // Code goes here
+    return text.split('').filter(char => 'aeiouAEIOU'.includes(char)).length;
 }
 
+// With replace and regex
+function vowelsCounter(text) {
+    return text.replace(/[^aeiou]/gi, '').length;
+}
 
+// With match and regex
+function vowelsCounter(text) {
+    return (text.match(/[aeiou]/gi)||[]).length;
+}
+
+// // With for loop
+// function vowelsCounter(text) {
+//     let count = 0;
+//     for (let i = 0, string = text.toLowerCase(); i < text.length; i++){
+//         if ('aeiou'.includes(string[i])){
+//             count++
+//         }
+//     }
+//     return count;
+// }
+
+// // With for of loop
+// function vowelsCounter(text) {
+//     let count = 0;
+//     for (let char of text.toLowerCase()){
+//         if ('aeiou'.includes(char)){
+//             count++
+//         }
+//     }
+//     return count;
+// }
 
 module.exports = vowelsCounter;
