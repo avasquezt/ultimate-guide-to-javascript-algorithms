a265866fcfc3ff0dd7cbab8880cd3957e760dfad
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-15 22:34:15 -0500

    Add capSentence methods

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -6,11 +6,32 @@ return the equivalent of the sentence when capitalised. E.g
 
 
 
-
+// With replace and RegExp
 function capSentence(text) {
    // Code goes here
+   return text.toLowerCase().replace(/\b./g, char => char.toUpperCase());
 }
 
+// With split and replace
+function capSentence(text) {
+  // Code goes here
+  return text.toLowerCase().split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+}
 
+// With forEach
+function capSentence(text) {
+  // Code goes here
+  let ans = [];
+  text.toLowerCase().split(' ').forEach(word =>  ans.push(word[0].toUpperCase() + word.slice(1)));
+  return ans.join(' ');
+}
+
+// map and replace
+function capSentence(text) {
+  // Code goes here
+  return text.toLowerCase().split(' ').map(word => word.replace(word[0], word[0].toUpperCase())).join(' ');
+}
+
+console.log(capSentence('the tales of scotch!'));
 
 module.exports = capSentence
