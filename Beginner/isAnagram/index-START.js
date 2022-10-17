466d6a61362f246994cc296a8d1138d94928b881
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-17 14:22:41 -0500

    Add isAnagram methods

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -5,11 +5,44 @@ don't. E.g
     isAnagram('silent', 'listen') // should return true
 */
 
-
-
+// With sort
 function isAnagram(stringA, stringB) {
-    // Code goes here
+    return stringA.split('').sort().join('') == stringB.split('').sort().join('');
 }
 
+// With replace
+function isAnagram(stringA, stringB) {
+    let notSeen = stringB.slice();
+    for(let char of stringA){
+        notSeen = notSeen.replace(char, '');
+    }
+    return notSeen.length == 0;
+}
+
+// With a map
+function isAnagram(stringA, stringB) {
+    function map(string){
+        let charMap = new Map();
+        for(let char of string){
+            charMap.set(char, charMap.get(char) + 1 || 1);
+        }
+        return charMap
+    }
+
+    let charMapA = map(stringA);
+    let charMapB = map(stringB);
+
+    if (charMapA.size != charMapB.size) return false;
+
+    for(let [char, val] of charMapA){
+        if(val !== charMapB.get(char)){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+console.log(isAnagram('silent', 'listen'));
 
 module.exports = isAnagram
