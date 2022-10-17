28fd21ab3afd55cf89b24b985539158c35de9c2f
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-17 13:02:12 -0500

    Add longestWord methods

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -4,10 +4,28 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
-
+// Reduce
 function longestWord(text) {
-    // Code goes here
+    return text.split(' ').reduce((acc, word) => word.length > acc.length ? word : acc);
 }
 
+// Sort
+function longestWord(text) {
+    return text.split(' ').sort((a, b) => b.length - a.length)[0];
+}
+
+// For loop
+function longestWord(text) {
+    let wordArray = text.split(' ');
+    let ans = '';
+    for(let i = 0; i < wordArray.length; i++){
+        if(wordArray[i].length > ans.length){
+            ans = wordArray[i];
+        }
+    }
+    return ans;
+}
+
+console.log(longestWord('Top Shelf Web Development Training on Scotch'));
 
 module.exports = longestWord
