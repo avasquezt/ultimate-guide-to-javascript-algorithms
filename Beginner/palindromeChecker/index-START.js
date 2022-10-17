bc427ee279902458ee8bc9b20c42214d0c1060b0
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-16 21:40:59 -0500

    Add palindromeChecker methods

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -3,13 +3,28 @@ Given a string of text, return true or false indicating whether or not the text 
 e.g palindromeChecker('racecar') // will return true
 */
 
-
-
-
+// reverse
 function palindromeChecker(text) {
-    v// Code goes here
+    // Code goes here
+    return text === text.split('').reverse().join('');
 }
 
+// With every
+function palindromeChecker(text) {
+    // Code goes here
+    return text.split('').every((char, i) => char === text.at(-(i+1)));
+}
 
+// With for
+function palindromeChecker(text) {
+    // Code goes here
+    for(let i = 0; i < text.length / 2; i++){
+        if(text[i] !== text.at(-(i+1))) return false;
+    }
+    return true;
+}
+
+console.log(palindromeChecker('text'))
+console.log(palindromeChecker('racecar'))
 
 module.exports = palindromeChecker;
