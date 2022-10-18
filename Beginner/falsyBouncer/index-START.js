c8cf582535ad7d9de384f972bf7e27cef7f847b7
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-18 16:25:32 -0500

    Add falsyBouncer methods

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,9 +5,18 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-
+// With filter
 function falsyBouncer(array) {
-    // Code goes here
+    return array.filter(n => n);
+}
+
+// With for
+function falsyBouncer(array) {
+    let result  = []
+    for(let value of array){
+        if(value) result.push(value);
+    }
+    return result;
 }
 
 
