cc5b3af6b9561532574a53c4e989291c8cc44f2c
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-18 16:06:33 -0500

    Add mergeArrays  methods

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,6 +4,7 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// With set
 function mergeArrays(...arrays) {
 
     let jointArray = []
@@ -17,5 +18,31 @@ function mergeArrays(...arrays) {
     
 }
 
+// With set and flat
+function mergeArrays(...arrays) {
+
+    let jointArray = arrays.flat();
+
+    return [...new Set([...jointArray])]
+
+    
+}
+
+// With filter and indexOf
+function mergeArrays(...arrays) {
+    return arrays.flat().filter((n, i, arr) => i == arr.indexOf(n));
+}
+
+
+// With reduce
+function mergeArrays(...arrays) {
+    return arrays.flat().reduce((acc, n) => acc.includes(n) ? acc : acc.concat(n), []);
+}
+
+console.log([Array.from(Array(Math.round(100*Math.random())), _ => Math.floor(9*Math.random()))],
+            [Array.from(Array(Math.round(100*Math.random())), _ => Math.floor(9*Math.random()))],
+            [Array.from(Array(Math.round(100*Math.random())), _ => Math.floor(9*Math.random()))],
+            [Array.from(Array(Math.round(100*Math.random())), _ => Math.floor(9*Math.random()))],
+            [Array.from(Array(Math.round(100*Math.random())), _ => Math.floor(9*Math.random()))])
 
 module.exports = mergeArrays
