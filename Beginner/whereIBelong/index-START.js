1fe0972d206bf99e4f8f7d4466bab585df454a85
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-18 23:00:45 -0500

    Add whereIBelong  methods

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -8,12 +8,38 @@ than 1(index 0), but less than 2(index 1).
 */
 
 
-
+// With findIndex
 function whereIBelong(arr, num) {
-   // Code goes here
-
+   arr.sort((a, b) => a - b);
+   return Math.max(arr.findIndex(n => num <= n), arr.length);
 }
 
+// With for
+function whereIBelong(arr, num) {
+   arr.sort((a, b) => a - b);
+   for(let i = 0; i < arr.length; i++){
+      if (num <= arr[i]) return i;
+   }
+   return arr.length;
+}
 
+// With for and not sort
+function whereIBelong(arr, num) {
+   let count = 0;
+   for(let i = 0; i < arr.length; i++){
+      if (num > arr[i]) count++;
+   }
+   return count;
+}
+
+// With while
+function whereIBelong(arr, num) {
+   arr.sort((a, b) => a - b);
+   let i = 0;
+   while(num > arr[i]){
+      i++;
+   }
+   return i;
+}
 
 module.exports = whereIBelong
